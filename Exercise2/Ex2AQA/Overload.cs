e74a6acb0a1da4b811fe812b7b71e68dea1bc03d
StilikS <stiliksqa@gmail.com>
2021-10-18 22:25:20 +0300

    Ex2

---
+++ Exercise2/Ex2AQA/Overload.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ex2AQA
+{
+    private class Overload
+    {
+        public void DisplayOverload(int a)
+        {
+            Console.WriteLine($"DisplayOverload: {a}");
+        }
+        
+        public void DisplayOverload(string a)
+    }
+}
---
+++ Exercise2/Ex2AQA/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Ex2AQA
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            //example 1
+            /*var overload = new Overload();
+            overload.DisplayOverload(100);
+            overload.DisplayOverload("metod overloading");
+            overload.DisplayOverload("metod overloading", 100);*/
+            
+            //example2 
+            /*var overland = new Overload();
+            overland.Display();*/
+            
+            //examole 3
+           /* ClassA a = new ClassA();
+            a.Display1();*/
+            
+            //examole 4
+            /*{
+                ClassB b = new ClassB();
+                b.Display2();
+            }*/
+            
+            //Example 5
+            /*{
+                ClassA classB = new ClassB();
+            }*/
+            
+            //Example 6
+            ClassA classA = new ClassC();
+            ClassB classB = new ClassC();
+            classA.XXX();
+            classB.XXX();
+        }
+    }
+}
