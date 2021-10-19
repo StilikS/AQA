6fe6ad41eff99889b8042fa39dafe623d1d018ea
StilikS <stiliksqa@gmail.com>
2021-10-19 23:56:59 +0300

    Ex2

--- Exercise2/Ex2AQA/Program.cs
+++ Exercise2/Ex2AQA/Program.cs
@@ -4,31 +4,26 @@
     {
         static void Main(string[] args)
         {
-            //example 1
+            //Example 1
             var overload = new Overload();
             overload.DisplayOverload(100);
             overload.DisplayOverload("metod overloading");
             overload.DisplayOverload("metod overloading", 100);
-            
-            //example2 
+            //Example 2 
            /* var overland = new Overload();
             overland.Display();*/
-            
-            //examole 3
+           //Example 3
             /*ClassA a = new ClassA();
             a.Display1();*/
-            
             //examole 4
             /*{
                 ClassB b = new ClassB();
                 b.Display2();
             }*/
-            
             //Example 5
             /*{
                 ClassA classB = new ClassB();
             }*/
-            
             //Example 6
             /*ClassA classA = new ClassC();
             ClassB classB = new ClassC();
