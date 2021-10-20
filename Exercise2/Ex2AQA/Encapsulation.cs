436bde83b7094a7a07bdf6f9952d2eed5c645d2d
StilikS <stiliksqa@gmail.com>
2021-10-20 18:54:46 +0300

    Ex2

---
+++ Exercise2/Ex2AQA/Encapsulation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Ex2AQA
+{
+    public class Encapsulation
+    {
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    age = value;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+        }
+    }
+}
--- Exercise2/Ex2AQA/Program.cs
+++ Exercise2/Ex2AQA/Program.cs
@@ -1,18 +1,21 @@
-﻿namespace Ex2AQA
+﻿using System;
+using Microsoft.VisualBasic;
+
+namespace Ex2AQA
 {
     class Program1
     {
         static void Main(string[] args)
         {
             //Example 1
-            var overload = new Overload();
+            /*var overload = new Overload();
             overload.DisplayOverload(100);
             overload.DisplayOverload("metod overloading");
-            overload.DisplayOverload("metod overloading", 100);
+            overload.DisplayOverload("metod overloading", 100);*/
             //Example 2 
-           /* var overland = new Overload();
-            overland.Display();*/
-           //Example 3
+            /* var overland = new Overload();
+             overland.Display();*/
+            //Example 3
             /*ClassA a = new ClassA();
             a.Display1();*/
             //examole 4
@@ -29,6 +32,11 @@
             ClassB classB = new ClassC();
             classA.XXX();
             classB.XXX();*/
+            //encapsulation
+            Encapsulation Encapsulation = new Encapsulation();
+            Console.WriteLine("Write your age: ");
+            Encapsulation.Age = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Your age: {Encapsulation.Age} ");
         }
     }
 }
