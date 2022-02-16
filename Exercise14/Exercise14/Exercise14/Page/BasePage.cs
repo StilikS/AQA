ca6052305f4a997dba05890382dfa1a823194b48
StilikS <stiliksqa@gmail.com>
2022-02-16 22:25:34 +0300

    ex14

---
+++ Exercise14/Exercise14/Exercise14/Page/BasePage.cs
@@ -0,0 +1,47 @@
+﻿namespace Exercise14.Page
+{
+    using System.Threading;
+    using NUnit.Framework;
+    using OpenQA.Selenium;
+
+    namespace PageObject.BaseEntities
+    {
+        public abstract class BasePage
+        {
+            protected IWebDriver Driver;
+            private int loadTime = 10;
+
+            public BasePage(IWebDriver driver, bool urlOpen)
+            {
+                Driver = driver;
+
+                if (urlOpen)
+                {
+                    OpenPage();
+                }
+
+                WaitForOpen();
+            }
+
+            protected abstract void OpenPage();
+            public abstract bool IsPageOpened();
+
+            protected void WaitForOpen()
+            {
+                var sec = 0;
+                var indicatorOpenedPage = IsPageOpened();
+                while (! indicatorOpenedPage && sec < loadTime)
+                {
+                    Thread.Sleep(2000);
+                    sec++;
+                    indicatorOpenedPage = IsPageOpened();
+                }
+
+                if (!indicatorOpenedPage)
+                {
+                    throw new AssertionException("Page was not opened.");
+                }
+            }
+        }
+    }
+}
---
+++ Exercise14/Exercise14/Exercise14/Page/LoginPage.cs
@@ -0,0 +1,54 @@
+﻿namespace Exercise14.Page
+{
+    using Exercise14.Page.PageObject.BaseEntities;
+    using System;
+    using OpenQA.Selenium;
+    using PageObject.BaseEntities;
+
+    namespace PageObject.Pages
+    {
+        public class LoginPage : BasePage
+        {
+            private  string END_POINT = "";
+
+            // Описание элементов
+            private  readonly By _userNameInputBy = By.Id("user-name");
+
+            private  readonly By _passwordInputBy = By.Id("password");
+
+            private  readonly By _logInButtonBy = By.Id("login-button");
+
+            // Инициализация класса
+            public LoginPage(IWebDriver driver, bool openPageByUrl) : base(driver, openPageByUrl)
+            {
+            }
+
+            public LoginPage(IWebDriver driver) : base(driver, false)
+            {
+            }
+
+            protected override void OpenPage()
+            {
+                Driver.Navigate().GoToUrl(BaseTest.BaseUrl + END_POINT);
+            }
+
+            public override bool IsPageOpened()
+            {
+                try
+                {
+                    return LogInButton.Displayed;
+                }
+
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+
+            // Методы
+            public IWebElement UserNameInput => Driver.FindElement(_userNameInputBy);
+            public IWebElement PasswordInput => Driver.FindElement(_passwordInputBy);
+            public IWebElement LogInButton => Driver.FindElement(_logInButtonBy);
+        }
+    }
+}
